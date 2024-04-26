b582c5361a38db40d4f429f038c7c61459866e7f
mmasondcroz <mmasondcroz@gmail.com>
2024-04-25 19:30:08 -0500

    New Updates of the Game

---
+++ wordList.js
@@ -0,0 +1,170 @@
+const wordList = [
+    {
+        word: "falda",
+        hint: "skirt"
+    },
+    {
+        word: "zapatos",
+        hint: "shoes"
+    },
+    {
+        word: "sandalias",
+        hint: "sandles"
+    },
+    {
+        word: "chanclas",
+        hint: "flip flops"
+    },
+    {
+        word: "botas",
+        hint: "boots"
+    },
+    {
+        word: "medias",
+        hint: "socks"
+    },
+    {
+        word: "sueter",
+        hint: "sweater"
+    },
+    {
+        word: "camisa",
+        hint: "shirt"
+    },
+    {
+        word: "camiseta",
+        hint: "t-shirt"
+    },
+    {
+        word: "sudadera",
+        hint: "sweatshirt"
+    },
+    {
+        word: "blusa",
+        hint: "blouse"
+    },
+    {
+        word: "chaqueta",
+        hint: "jacket"
+    },
+    {
+        word: "gafas",
+        hint: "glasses"
+    },
+    {
+        word: "GafasDeSol",
+        hint: "sunglasses"
+    },
+    {
+        word: "pantalones",
+        hint: "pants"
+    },
+    {
+        word: "cortos",
+        hint: "shorts"
+    },
+    {
+        word: "vestido",
+        hint: "dress"
+    },
+    {
+        word: "traje",
+        hint: "suit"
+    },
+    {
+        word: "sombrero",
+        hint: "hat"
+    },
+    {
+        word: "gorro",
+        hint: "beanie / tuque"
+    },
+    {
+        word: "gorra",
+        hint: "baseball cap"
+    },
+    {
+        word: "reloj",
+        hint: "watch"
+    },
+    {
+        word: "cinturon",
+        hint: "belt"
+    },
+    {
+        word: "chaleco",
+        hint: "vest"
+    },
+    {
+        word: "bufanda",
+        hint: "scarf"
+    },
+    {
+        word: "abrigo",
+        hint: "coat"
+    },
+    {
+        word: "guantes",
+        hint: "gloves"
+    },
+    {
+        word: "calzoncillos",
+        hint: "underwear"
+    },
+    {
+        word: "MePongo",
+        hint: "I put on"
+    },
+    {
+        word: "llevo",
+        hint: "I am wearing"
+    },
+    {
+        word: "TePones",
+        hint: "You put on"
+    },
+    {
+        word: "llevas",
+        hint: "You are wearing"
+    },
+    {
+        word: "SePone",
+        hint: "He/She puts on"
+    },
+    {
+        word: "lleva",
+        hint: "He/she is wearing"
+    },
+    {
+        word: "MeQuedaBien",
+        hint: "It fits me well"
+    },
+    {
+        word: "MeQuedaMal",
+        hint: "It fits me poorly"
+    },
+    {
+        word: "LeQuedaBien",
+        hint: "It fits them well"
+    },
+    {
+        word: "LeQuedaMal",
+        hint: "It fits them poorly"
+    },
+    {
+        word: "MeQuedaGrande",
+        hint: "It fits me big"
+    },
+    {
+        word: "MeQuedaPequeno",
+        hint: "It fits me small"
+    },
+    {
+        word: "LeQuedaGrande",
+        hint: "It fits them big"
+    },
+    {
+        word: "LeQuedaPequeno",
+        hint: "It fits them small"
+    },
+]
